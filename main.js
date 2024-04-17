2181e45b24ca28809f1a1ae444521d5f073a80c2
Rahimashaikh <160630789+Rahimashaikh@users.noreply.github.com>
2024-04-17 23:21:51 +0500

    Add files via upload
    its a simple word counter project

---
+++ main.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: " please enter your sentence to count your words: ",
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+console.log(words);
+console.log(`your count words are ${words.length}`);
